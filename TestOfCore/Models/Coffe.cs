82bf119941852f44a713fa4486939ff997106883
Sebastian Donastor <sebastiandonastor@gmail.com>
2019-03-04 21:18:11 -0400

    Agregating coffe entity

---
+++ TestOfCore/Models/Coffe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestOfCore.Models
+{
+    public class Coffe
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
